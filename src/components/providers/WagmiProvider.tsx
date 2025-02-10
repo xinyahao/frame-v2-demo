8dceb9d3e1c9f8afeacf3877b9cccb958aa854ce
xinya <xinya@xinyadeMacBook-Pro.local>
2025-02-10 15:57:54 +0800

    first

---
+++ src/components/providers/WagmiProvider.tsx
@@ -0,0 +1,23 @@
+import { createConfig, http, WagmiProvider } from "wagmi";
+import { base, optimism } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
+
+export const config = createConfig({
+  chains: [base, optimism],
+  transports: {
+    [base.id]: http(),
+    [optimism.id]: http(),
+  },
+  connectors: [farcasterFrame()],
+});
+
+const queryClient = new QueryClient();
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
+}
